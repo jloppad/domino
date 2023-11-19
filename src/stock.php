dde4a9cd30380fc2b60fcd8b1164703c55f41edc
jloppad <jloppad@g.educaand.es>
2023-11-19 12:22:29 +0100

    stock.php works

--- src/stock.php
+++ src/stock.php
@@ -1,5 +1,52 @@
 <?php
 
-class Stock {
+class stock
+{
+    private array $tiles;
+
+    /**
+     * @throws Exception
+     */
+    public function __construct()
+    {
+        $this->generateTiles();
+        $this->shuffleTiles();
+    }
+
+    /**
+     * @throws Exception
+     */
+    private function generateTiles(): void
+    {
+        $this->tiles = [];
+        for ($i = 0; $i <= 6; $i++) {
+            for ($j = $i; $j <= 6; $j++) {
+                $tile = new tile($i, $j);
+                $this->tiles[] = $tile;
+            }
+        }
+    }
+
+    private function shuffleTiles(): void
+    {
+        shuffle($this->tiles);
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function getTile()
+    {
+        if ($this->isEmpty()) {
+            throw new Exception("The stock is empty");
+        }
+
+        return array_pop($this->tiles);
+    }
+
+    public function isEmpty(): bool
+    {
+        return empty($this->tiles);
+    }
 
 }
--- src/test.php
+++ src/test.php
@@ -2,7 +2,24 @@
 
 require_once '../vendor/autoload.php';
 
+try {
 
-    $tile = new tile(3, 5);
-    var_dump($tile);
+    // Class tile
+        $tile = new tile(3, 5);
+        var_dump($tile);
+
+    // Class Stock
+        $stock = new stock();
+        var_dump($stock);
+        var_dump($stock->getTile());
+        /* Empty the stock
+        for ($i = 0; $i < 27; $i++) {
+            var_dump($stock->getTile());
+        }
+        */
+        var_dump($stock->isEmpty());
+
+} catch (Exception $e) {
+    echo ($e);
+}
 
