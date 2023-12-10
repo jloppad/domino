56ff0eaf2acb874efd4364d078121ec6cdb262aa
jloppad <jloppad@g.educaand.es>
2023-12-10 23:15:44 +0100

    Idk how to do the start of the game

--- src/domino.php
+++ src/domino.php
@@ -16,6 +16,7 @@ header("refresh:5");
 $username = $_SESSION['username'];
 $game = $_SESSION['game'];
 $stock = $game->getStock();
+$table = $game->getTable();
 $usernames = $game->getUsernames();
 $players = $game->getPlayers();
 
@@ -26,6 +27,18 @@ if (!isset($_SESSION['initialSetup'])) {
         }
     }
     $_SESSION['initialSetup'] = true;
+}
+$double = new Tile(0,0);
+foreach ($usernames as $username) {
+    $tiles = $players[$username]->getTiles();
+    foreach ($tiles as $tile) {
+        if ($tile->getLeftPips() == $tile->getRightPips() && $double->countPips() < $tile->countPips()){
+            $double = $tile;
+            $game->setActualUsername($username);
+            $game->setHandUsername($username);
+            $game->setSelectedTile($double);
+        }
+    }
 }
 ?>
 
@@ -39,7 +52,12 @@ if (!isset($_SESSION['initialSetup'])) {
 <body>
 <form method='post'>
     <h1>Blas's Domino</h1>
-
+    <?php
+        $tilesTable = $table->getTiles();
+        foreach ($tilesTable as $tileTable){
+            echo '<img src="../resources/tiles/' . $tileTable->getLeftPips() . '_' . $tileTable->getRightPips() . '@0.5x.png">';
+        }
+    ?>
     <div class="players">
         <table>
             <tr>
@@ -55,10 +73,14 @@ if (!isset($_SESSION['initialSetup'])) {
                     echo "<td>";
                     $tiles = $players[$username]->getTiles();
                     foreach ($tiles as $tile) {
+                        $mark = "";
+                        if ($double == $tile){
+                            $mark = "class='mark'";
+                        }
                         if ($_SESSION['username'] != $username) {
                             echo '<img src="../resources/tiles/hide@0.5x.png" alt="hide">';
                         } else {
-                            echo '<img src="../resources/tiles/' . $tile->getLeftPips() . '_' . $tile->getRightPips() . '@0.5x.png">';
+                            echo '<img src="../resources/tiles/' . $tile->getLeftPips() . '_' . $tile->getRightPips() . '@0.5x.png" ' . $mark . '>';
                         }
                     }
                     echo "</td>";
